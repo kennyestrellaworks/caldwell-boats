8fda3dcec7362cc4523bb29db454e21071c54dfa
kennyestrellaworks <kennyestrellaworks@gmail.com>
2025-07-21 02:16:57 +0800

    TypeScript practices.

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,14 +1,4 @@
-import * as React from "react";
-
-type PageHeadingProps = {
-  headingTitle: string;
-  headingParagraph: string;
-};
-
-export const PageHeading: React.FC<PageHeadingProps> = ({
-  headingTitle,
-  headingParagraph,
-}) => {
+export const PageHeading = ({ headingTitle, headingParagraph }) => {
   return (
     <section className="relative py-20 bg-gradient-to-r from-primary-800 to-ocean-700 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
